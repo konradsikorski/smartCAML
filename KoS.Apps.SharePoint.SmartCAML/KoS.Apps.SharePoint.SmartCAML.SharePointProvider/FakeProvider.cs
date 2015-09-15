2bbe22ca55a031b938acbae6c1f9fa786f79ce41
Konrad Sikorski <konrad.sikorski@windowslive.com>
2015-09-15 20:23:36 +0200

    add missing files

---
+++ KoS.Apps.SharePoint.SmartCAML/KoS.Apps.SharePoint.SmartCAML.SharePointProvider/FakeProvider.cs
@@ -0,0 +1,45 @@
+﻿using KoS.Apps.SharePoint.SmartCAML.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoS.Apps.SharePoint.SmartCAML.SharePointProvider
+{
+    public class FakeProvider : ISharePointProvider
+    {
+        public Web Web { get; private set; }
+
+        public Web Connect(string url)
+        {
+            Web = GetWeb(url);
+            return Web;
+        }
+
+        private Web GetWeb(string url)
+        {
+            return new Web
+            {
+                Title = "Test",
+                Url = url,
+                Lists = GetLists().ToList()
+            };
+        }
+
+        private IEnumerable<SharePointList> GetLists()
+        {
+            return new List<SharePointList>
+            {
+                new SharePointList { Id = 1, Name = "List1" },
+                new SharePointList { Id = 2, Name = "List2" },
+                new SharePointList { Id = 3, Name = "List3" },
+                new SharePointList { Id = 4, Name = "List4" },
+                new SharePointList { Id = 5, Name = "List5" },
+                new SharePointList { Id = 6, Name = "List6" },
+                new SharePointList { Id = 7, Name = "List7" },
+                new SharePointList { Id = 8, Name = "List8" },
+            };
+        }
+    }
+}
---
+++ KoS.Apps.SharePoint.SmartCAML/KoS.Apps.SharePoint.SmartCAML.SharePointProvider/SharePointProviderFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoS.Apps.SharePoint.SmartCAML.SharePointProvider
+{
+    public enum SharePointProviderType
+    {
+        Fake = 0,
+        SharePoint2010ServerModel = 1,
+        SharePoint2010ClientModel = 2,
+        SharePoint2013ServerModel = 3,
+        SharePoint2013ClientModel = 4
+    }
+    public static class SharePointProviderFactory
+    {
+        public static ISharePointProvider Create(SharePointProviderType type)
+        {
+            switch (type)
+            {
+                case SharePointProviderType.Fake: return new FakeProvider();
+                case SharePointProviderType.SharePoint2010ServerModel:
+                case SharePointProviderType.SharePoint2010ClientModel:
+                case SharePointProviderType.SharePoint2013ServerModel:
+                case SharePointProviderType.SharePoint2013ClientModel:
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
