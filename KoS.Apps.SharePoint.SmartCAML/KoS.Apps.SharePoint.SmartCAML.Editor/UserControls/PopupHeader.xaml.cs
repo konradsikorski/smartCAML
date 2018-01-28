1c2350dfa78dd826ddac2e0c8331225c7e759436
Konrad Sikorski <konrad.sikorski@windowslive.com>
2018-01-28 17:46:13 +0100

    popupHeader and popup border

---
+++ KoS.Apps.SharePoint.SmartCAML/KoS.Apps.SharePoint.SmartCAML.Editor/UserControls/PopupHeader.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KoS.Apps.SharePoint.SmartCAML.Editor.UserControls
+{
+    public partial class PopupHeader : UserControl
+    {
+        public event Action<object, RoutedEventArgs> Closed;
+
+        public PopupHeader()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string),
+            typeof(PopupHeader), new PropertyMetadata(string.Empty));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Closed?.Invoke(sender, e);
+        }
+    }
+}
